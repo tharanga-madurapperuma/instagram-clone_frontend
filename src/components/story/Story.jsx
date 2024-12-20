714b40362614b10b4df8f914c6a9bdcd7df7b24e
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-20 23:20:23 +0530

    get data from the backend

--- src/components/story/Story.jsx
+++ src/components/story/Story.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import Images from "../../assets/images";
 import "./story.css";
 
-const Story = () => {
+const Story = ({ story }) => {
+    const storyCliked = () => {
+        console.log(story);
+    };
     return (
         <div className="story m-2 mt-10">
-            <div className="story_outline flex justify-center items-center">
-                <div className="story_image bg-white">
+            <div
+                className={
+                    story.watched
+                        ? "story_outline_visited flex justify-center items-center"
+                        : "story_outline flex justify-center items-center"
+                }
+            >
+                <div
+                    className="story_image bg-white"
+                    onClick={() => {
+                        storyCliked();
+                    }}
+                >
                     <img src={Images.tree} alt="story" />
                 </div>
             </div>
