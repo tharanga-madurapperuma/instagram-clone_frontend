00c2091e2d185c045b4c6f779038df1c0f9c82e0
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-22 12:47:17 +0530

    Successfully add a progress bar to the story and timout

--- src/components/watchStory/WatchStory.jsx
+++ src/components/watchStory/WatchStory.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./watchStory.css";
 import Data from "../../fetchData";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const WatchStory = ({ story, timeOut }) => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const duration = timeOut; // Duration of the story (in ms)
+        const intervalTime = 10; // Time between each increment (in ms)
+        const increment = 100 / (duration / intervalTime); // Increment value per interval
+
+        const interval = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress < 100) {
+                    return prevProgress + increment;
+                } else {
+                    clearInterval(interval);
+                    return 100;
+                }
+            });
+        }, intervalTime);
+
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
+    console.log(progress);
+    useEffect(() => {
+        document.querySelector(".barCompleted").style.width = `${progress}%`;
+    }, [progress]);
+
     return (
         <div className="watch-story-container flex flex-col h-[65vh]">
             <p>{story.description}</p>
+
+            {/* Story visible time progress bar */}
+            <div className="flex w-full">
+                <ProgressBar
+                    completed={progress}
+                    className="wrapper"
+                    barContainerClassName="container"
+                    completedClassName="barCompleted"
+                    isLabelVisible={false} // Removes the text label
+                />
+            </div>
 
             <div className="story-image-container">
                 <img
