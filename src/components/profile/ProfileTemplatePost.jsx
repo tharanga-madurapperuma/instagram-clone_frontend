fe619fbb10e2a451af149b5294b98dfddbd93212
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-23 17:25:25 +0530

    Update user template for posts

--- src/components/profile/ProfileTemplatePost.jsx
+++ src/components/profile/ProfileTemplatePost.jsx
@@ -45,20 +45,24 @@ const ProfileTemplatePost = ({ user, post }) => {
             <div className="content-image">
                 <div className="image">
                     <img
-                        src={`${Data.fileStore.downloadUser}${user.userImage}`}
+                        src={
+                            user?.userImage
+                                ? `${Data.fileStore.downloadUser}${user?.userImage}`
+                                : `${Data.fileStore.downloadUser}general.jpg_20241223164338_a9ea9a8a_user.jpg`
+                        }
                     ></img>
                 </div>
             </div>
             <div className="content_data ml-3">
                 <div className="data-user-name-date flex items-center">
                     <div className="username text-base font-medium">
-                        {user.firstName} {user.lastName}
+                        {user?.firstName} {user?.lastName}
                     </div>
                     <div className="mx-2 bg-gray-500 mt-1"></div>
                     <div className="date text-gray-500 ml-0">{displayTime}</div>
                 </div>
                 <div className="data-caption">
-                    <p className="text-sm font-light">{user.caption}</p>
+                    <p className="text-sm font-light">{user?.caption}</p>
                 </div>
             </div>
         </div>
