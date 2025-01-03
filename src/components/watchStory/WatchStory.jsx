f536ab2222490e82aad7edf77409593fe5f405be
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2025-01-03 17:53:03 +0530

    delete story implement

--- src/components/watchStory/WatchStory.jsx
+++ src/components/watchStory/WatchStory.jsx
@@ -4,10 +4,12 @@ import Data from "../../fetchData";
 import ProgressBar from "@ramonak/react-progress-bar";
 import axios from "axios";
 import ProfileTemplate from "../profile/ProfileTemplate";
+import { CiMenuKebab } from "react-icons/ci";
 
 const WatchStory = ({ story, timeOut }) => {
     const [progress, setProgress] = useState(0);
     const [user, setUser] = useState();
+    const [deleteClicked, setDeleteClicked] = useState(false);
 
     useEffect(() => {
         const duration = timeOut; // Duration of the story (in ms)
@@ -43,9 +45,44 @@ const WatchStory = ({ story, timeOut }) => {
         document.querySelector(".barCompleted").style.width = `${progress}%`;
     }, [progress]);
 
+    const handleDelete = async () => {
+        try {
+            const response = await axios.delete(
+                Data.stories.deleteStory + story.storyId
+            );
+            if (response.status === 200) {
+                alert("Story deleted successfully!");
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error("Error deleting story:", error);
+            alert("Failed to delete story. Please try again.");
+        }
+    };
+
     return (
         <div className="watch-story-container flex flex-col h-[65vh]">
-            <ProfileTemplate user={user} />
+            <div className="flex items-center justify-between story-delete-menu">
+                <ProfileTemplate user={user} />
+                <CiMenuKebab
+                    className="ml-5 cursor-pointer"
+                    onClick={() => {
+                        deleteClicked
+                            ? setDeleteClicked(false)
+                            : setDeleteClicked(true);
+                    }}
+                />
+                <ul
+                    className={
+                        deleteClicked
+                            ? "story-delete-menu-list-active"
+                            : "story-delete-menu-list-inactive"
+                    }
+                >
+                    <li onClick={handleDelete}>Delete</li>
+                </ul>
+            </div>
+
             <p className="mt-5">{story.description}</p>
 
             {/* Story visible time progress bar */}
