e89792cb300dc438fa76c2143ddaf2bef2ba57fd
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2025-01-12 09:54:38 +0530

    make color changing navbar when scrolling

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Images from "../../assets/images";
+import React, { useEffect, useRef, useState } from "react";
 import "./home.css";
 import Story from "../../components/story/Story";
 import Post from "../../components/post/Post";
@@ -18,8 +17,11 @@ import {
 import { CgAddR, CgProfile } from "react-icons/cg";
 import { LuLogOut } from "react-icons/lu";
 import { TiThMenu } from "react-icons/ti";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
 
 const Home = () => {
+    // logo Image
+    const logo = "/assets/Logo.png";
     // get the user
     const [LOGGED_USER, setLoggedUser] = useState();
 
@@ -31,6 +33,8 @@ const Home = () => {
     const [stories, setStories] = useState([]);
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     const [menuOpen, setMenuOpen] = useState(false);
+    const [opacity, setOpacity] = useState(0);
+    const scrollableDivRef = useRef(null); // Reference to the scrollable div
 
     useEffect(() => {
         const handleResize = () => {
@@ -85,6 +89,38 @@ const Home = () => {
         setCloseModal(true);
     }, [closeModal]);
 
+    // Use effect for color changing navbar when scrolling
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollDiv = scrollableDivRef.current;
+            if (!scrollDiv) return;
+
+            const scrollY = scrollDiv.scrollTop; // Get scroll position of the div
+            console.log("Scroll position:", scrollY);
+
+            const maxOpacity = 0.9; // Maximum opacity value
+            const minOpacity = 0; // Minimum opacity value
+            const scrollThreshold = 600; // Scroll threshold to reach full opacity
+
+            const newOpacity = Math.min(
+                maxOpacity,
+                Math.max(minOpacity, scrollY / scrollThreshold)
+            );
+            setOpacity(newOpacity);
+        };
+
+        const scrollDiv = scrollableDivRef.current;
+        if (scrollDiv) {
+            scrollDiv.addEventListener("scroll", handleScroll);
+        }
+
+        return () => {
+            if (scrollDiv) {
+                scrollDiv.removeEventListener("scroll", handleScroll);
+            }
+        };
+    }, []);
+
     return (
         <div className="flex flex-row">
             {screenWidth > 768 ? (
@@ -92,7 +128,7 @@ const Home = () => {
                     {/* left menu */}
                     <div className="mt-0 mb-20 cursor-pointer">
                         <img
-                            src={Images.logo}
+                            src={logo}
                             alt="logo"
                             onClick={() => {
                                 navigation("/");
@@ -201,16 +237,10 @@ const Home = () => {
                             className=""
                             onClick={() => {
                                 setModalIsOpen(true);
+                                setMenuOpen(false);
                             }}
                         >
-                            <CgAddR className="home_icons" />
-                            <CreatePost
-                                open={modalIsOpen}
-                                onClose={() => {
-                                    setCloseModal(false);
-                                }}
-                                loggedUser={LOGGED_USER}
-                            />
+                            <CgAddR className="home_icons cursor-pointer" />
                         </div>
                         <div
                             className=""
@@ -239,13 +269,42 @@ const Home = () => {
                     </div>
                 </>
             ) : (
-                <TiThMenu
-                    className="home_icons menu-icon-smallScreen"
-                    onClick={() => {
-                        console.log("menu icon clicked");
-                        setMenuOpen(true);
-                    }}
-                />
+                <>
+                    <CreatePost
+                        open={modalIsOpen}
+                        onClose={() => {
+                            setCloseModal(false);
+                        }}
+                        loggedUser={LOGGED_USER}
+                        onOpen={() => {
+                            setMenuOpen(false);
+                        }}
+                    />
+                    <div
+                        className="small-screen-navbar flex justify-between items-center w-[100vw] h-10"
+                        style={{
+                            backgroundColor: `rgba(200,200,200, ${opacity})`,
+                            transition: "background-color 0.5s ease",
+                        }}
+                    >
+                        <div>
+                            <TiThMenu
+                                className="home_icons menu-icon-smallScreen"
+                                onClick={() => {
+                                    console.log("menu icon clicked");
+                                    setMenuOpen(true);
+                                }}
+                            />
+                        </div>
+
+                        <div>
+                            <img src={logo} alt="logo" />
+                        </div>
+                        <div>
+                            <AiOutlineUsergroupAdd className="small-screen-follower-icon" />
+                        </div>
+                    </div>
+                </>
             )}
 
             {/* seperation line */}
@@ -253,7 +312,10 @@ const Home = () => {
                 <div className="speration-line w-0.5 bg-gray-300"></div>
             ) : null}
 
-            <div className="feedSection justify-items-center ">
+            <div
+                className="feedSection justify-items-center "
+                ref={scrollableDivRef}
+            >
                 {/* feed section */}
                 <div className="feedSection_story flex flex-row justify-items-start ">
                     {[...stories].reverse().map((story) => (
