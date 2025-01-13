2a386a3887fc758b946b29103a9bc52e483be33d
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2025-01-13 11:29:20 +0530

    remove errors

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ProfileDetails from '../Components/ProfileDetails'
-import UserPost from '../Components/UserPost'
+import React from "react";
+import ProfileDetails from "../components/ProfileDetails";
+import UserPost from "../components/UserPost";
 
 const Profile = () => {
-  return (
-    <div className='px-20'>
-      <div>
-        <ProfileDetails />
-      </div>
-      <div>
-        <UserPost />
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="px-20">
+            <div>
+                <ProfileDetails />
+            </div>
+            <div>
+                <UserPost />
+            </div>
+        </div>
+    );
+};
 
-export default Profile
+export default Profile;
