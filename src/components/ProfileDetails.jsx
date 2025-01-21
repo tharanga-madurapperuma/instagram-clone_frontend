9187c960c61254c108e3c05dd75e948e1f7ce301
harshana2001 <1778harsh2001@gmail.com>
2025-01-21 22:12:46 +0530

    Implementing a mockup followers array to test the window

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -3,8 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 const ProfileDetails = () => {
     const settingIcon = "/assets/icons/Options.png";
-    const logo = "/assets/images/Logo.png";
-    const navigation = useNavigate();
+
+    const followers = [
+        { id: 1, username: 'tharu_rd', name: 'Tharushi Ranasinghe', avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=50&h=50&fit=crop' },
+        { id: 2, username: 'tharukaamasha', name: 'Tharuka Amasha', avatar: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=50&h=50&fit=crop' },
+        { id: 3, username: 'w.msew', name: 'W.M Sew wasala', avatar: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=50&h=50&fit=crop' },
+        { id: 4, username: 'waruna_hashan_', name: 'Waruna Hashan', avatar: 'https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=50&h=50&fit=crop' },
+        { id: 5, username: '.mindii', name: 'Theekshana lakmindu', avatar: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=50&h=50&fit=crop' },
+      ];
+    
     return (
         <div className="flex items-start space-x-20 mb-10 px-10 py-10">
           <img
